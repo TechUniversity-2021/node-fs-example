54e7beae2fd2721c8386a390e51cb6c3e750ce3c
Anukriti <anukritigarg13@gmail.com>
2021-02-16 19:35:51 +0530

    feat(node-fs-solution): Promisify fs functions

---
+++ utilities/PromisifiedFsFunctions.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+const promisifyReadFile = (filePath) => new Promise((resolve, reject) => {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(data);
+    }
+  });
+});
+
+const promisifyReadDir = (dirPath) => new Promise((resolve, reject) => {
+  fs.readdir(dirPath, 'utf-8', (err, files) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(files);
+    }
+  });
+});
+
+module.exports = {
+  promisifyReadFile,
+  promisifyReadDir,
+};
---
+++ utilities/PromisifiedFsFunctions.test.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const { promisifyReadFile, promisifyReadDir } = require('./PromisifiedFsFunctions');
+
+test('Unit testing for resolved', (done) => {
+  jest
+    .spyOn(fs, 'readFile')
+    .mockImplementation((file, option, callback) => {
+      callback(null, '21,34,43,57,"Anukriti"');
+    });
+  promisifyReadFile('randomFile').then((text) => {
+    expect(text).toBe('21,34,43,57,"Anukriti"');
+    done();
+  });
+});
+
+test('Unit testing for resolved using .resolves', () => {
+  jest
+    .spyOn(fs, 'readFile')
+    .mockImplementation((file, option, callback) => {
+      callback(null, '21,34,43,57,"Anukriti"');
+    });
+  return expect(promisifyReadFile('randomFile')).resolves.toBe(
+    '21,34,43,57,"Anukriti"',
+  );
+});
+
+test('Unit testing for rejected', () => {
+  jest
+    .spyOn(fs, 'readFile')
+    .mockImplementation((file, option, callback) => {
+      callback(new Error('An error occured'), null);
+    });
+  return expect(promisifyReadFile('randomFile')).rejects.toEqual(
+    new Error('An error occured'),
+  );
+});
+
+test('Unit test for promisifying fs.readdir resolved', () => {
+  jest
+    .spyOn(fs, 'readdir')
+    .mockImplementation((directory, option, callback) => {
+      callback(null, ['beverages.txt', 'fruits.txt', 'vegetables.txt']);
+    });
+  return expect(promisifyReadDir('randomDir')).resolves.toEqual(
+    ['beverages.txt', 'fruits.txt', 'vegetables.txt'],
+  );
+});
+
+test('Unit test for promisifying fs.readdir rejected', () => {
+  jest
+    .spyOn(fs, 'readdir')
+    .mockImplementation((directory, option, callback) => {
+      callback(new Error('Directory not found!'), null);
+    });
+  return expect(promisifyReadDir('randomDir')).rejects.toEqual(
+    new Error('Directory not found!'),
+  );
+});

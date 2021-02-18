e89275adac577a83431abcec3922aaac2d85977e
anjali0508 <46600043+anjali0508@users.noreply.github.com>
2021-02-18 08:21:47 +0530

    feat(file-utils): add read file, read directory functionality

---
+++ file-utils/fileUtils.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+function getFileData(fileName) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(fileName, "utf-8", (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
+  });
+}
+
+function getDirectoryFiles(dirName) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(dirName, "utf-8", (err, files) => {
+      if (err) reject(err);
+      resolve(files);
+    });
+  });
+}
+
+//getFileData("./test.txt").then(console.log, console.log);
+module.exports = { getFileData, getDirectoryFiles };

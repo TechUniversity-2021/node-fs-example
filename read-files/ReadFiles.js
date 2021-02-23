262a18426bcb659a51f36e8f93735dcaa8aa8c51
Anukriti <anukritigarg13@gmail.com>
2021-02-23 13:43:34 +0530

    feat(node-fs-solution): Return object with keys as arrays starting with given letter

--- read-files/ReadFiles.js
+++ read-files/ReadFiles.js
@@ -2,13 +2,15 @@ const path = require('path');
 const fsFunctions = require('../utilities/PromisifiedFsFunctions');
 const formatFunctions = require('../format-file-data/FormatFileData');
 
-const readFiles = async (directory) => {
+const readFiles = async (directory, startingChar) => {
   try {
     const fileArray = await fsFunctions.readDir(directory);
     const fileNames = fileArray.map((fileName) => path.parse(fileName).name);
     const filesData = await Promise.all(fileArray.map((fileName) => fsFunctions.readFile(`${directory}/${fileName}`)));
     const fileObject = filesData.reduce((accumulator, fileContent, index) => {
-      const fileValue = formatFunctions.splitIntoArray(fileContent);
+      let fileValue;
+      if (!startingChar) fileValue = formatFunctions.splitIntoArray(fileContent);
+      else fileValue = formatFunctions.splitIntoArray(fileContent, startingChar);
       return {
         ...accumulator,
         [fileNames[index]]: fileValue,
--- read-files/Readfiles.test.js
+++ read-files/Readfiles.test.js
@@ -7,17 +7,17 @@ describe('readFiles function', () => {
   it('should return object with key as filename and value as array of content', async () => {
     const spyOnReadFile = jest.spyOn(fsFunctions, 'readFile');
     const spyOnReadDir = jest.spyOn(fsFunctions, 'readDir');
-    const spyOnsplitIntoArray = jest.spyOn(formatFunctions, 'splitIntoArray');
+    const spyOnSplitIntoArray = jest.spyOn(formatFunctions, 'splitIntoArray');
 
     spyOnReadDir.mockResolvedValue(['MOCK_FILE.txt']);
     spyOnReadFile.mockResolvedValue('hi \nhello');
-    spyOnsplitIntoArray.mockImplementation((data) => ['hi', 'hello']);
+    spyOnSplitIntoArray.mockImplementation((data) => ['hi', 'hello']);
 
     const result = await solution.readFiles('MOCK_DIRECTORY');
 
     expect(spyOnReadDir).toHaveBeenCalledWith('MOCK_DIRECTORY');
     expect(spyOnReadFile).toHaveBeenCalledWith('MOCK_DIRECTORY/MOCK_FILE.txt');
-    expect(spyOnsplitIntoArray).toHaveBeenCalledWith('hi \nhello');
+    expect(spyOnSplitIntoArray).toHaveBeenCalledWith('hi \nhello');
 
     expect(result).toEqual({
       MOCK_FILE: ['hi', 'hello'],
@@ -50,5 +50,24 @@ describe('readFiles function', () => {
     } catch (error) {
       expect(error.message).toBe('Problem accessing files');
     }
+  });
+  it('should return object with key as filename and value as array of words starting with only the provided letter', async () => {
+    const spyOnReadFile = jest.spyOn(fsFunctions, 'readFile');
+    const spyOnReadDir = jest.spyOn(fsFunctions, 'readDir');
+    const spyOnSplitIntoArray = jest.spyOn(formatFunctions, 'splitIntoArray');
+
+    spyOnReadDir.mockResolvedValue(['MOCK_FILE.txt']);
+    spyOnReadFile.mockResolvedValue('hi \nhello\ngood');
+    spyOnSplitIntoArray.mockImplementation((data) => ['hi', 'hello']);
+
+    const result = await solution.readFiles('MOCK_DIRECTORY', 'h');
+
+    expect(spyOnReadDir).toHaveBeenCalledWith('MOCK_DIRECTORY');
+    expect(spyOnReadFile).toHaveBeenCalledWith('MOCK_DIRECTORY/MOCK_FILE.txt');
+    expect(spyOnSplitIntoArray).toHaveBeenCalledWith('hi \nhello');
+
+    expect(result).toEqual({
+      MOCK_FILE: ['hi', 'hello'],
+    });
   });
 });

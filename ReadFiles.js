76840c7cf6ba408bb50de7a170598a6faa47f2de
isha2912 <ishadeep222@gmail.com>
2021-02-25 08:58:08 +0530

    feat: add the next part

--- ReadFiles.js
+++ ReadFiles.js
@@ -3,23 +3,19 @@ const fileOps = require('./AsyncRead.js');
 const fileFilter = require('./filterData.js');
 
 const readFiles = async (directory, startingChar) => {
-  try {
-    const files = await fileOps.readDir(directory);
-    const fileNames = files.map((fileName) => path.parse(fileName).name);
-    const filesData = await Promise.all(files.map((fileName) => fileOps.readFile(`${directory}/${fileName}`)));
-    const fileObj = {};
-    let i = 0;
-    filesData.forEach((file) => {
-      const fileValue = fileFilter.splitByStartChar(file, startingChar);
-      fileObj[fileNames[i]] = fileValue;
-      i += 1;
-    });
+  const files = await fileOps.readDir(directory);
+  const fileNames = files.map((fileName) => path.parse(fileName).name);
+  const filesData = await Promise.all(files.map((fileName) => fileOps.readFile(`${directory}/${fileName}`)));
+  const fileObj = {};
+  let i = 0;
+  filesData.forEach((file) => {
+    const fileValue = fileFilter.splitByStartChar(file, startingChar);
+    fileObj[fileNames[i]] = fileValue;
+    i += 1;
+  });
 
-    console.log(fileObj);
-    return fileObj;
-  } catch (error) {
-    throw new Error('Problem accessing files');
-  }
+  console.log(fileObj);
+  return fileObj;
 };
 
 readFiles('./seed', 'k');

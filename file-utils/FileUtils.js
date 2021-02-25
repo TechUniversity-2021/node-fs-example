2de9bed252b4c170e9bb0ea3c980485ad8a0c0cd
srishtirathi <srishti.cs17@bmsce.ac.in>
2021-02-25 08:07:34 +0530

    refactor(file-utils): format code

--- file-utils/FileUtils.js
+++ file-utils/FileUtils.js
@@ -1,36 +1,30 @@
-const { rejects } = require("assert")
-const fs=require("fs")
+const fs = require('fs');
 
-function getDirectoryFiles(dirName){
-    return new Promise((resolve,reject)=>{
-        fs.readdir(dirName,(err,fileNames)=>{
-            if(err){
-                reject(err)
-
-            }
-            else{
-                resolve(fileNames)
-            }
-
-        })
-    })
-
+function getDirectoryFiles(dirName) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(dirName, (err, fileNames) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(fileNames);
+      }
+    });
+  });
 }
 
-function getData(fileName){
-    return new Promise((resolve,reject)=>{
-    fs.readFile(fileName,"utf-8",(err,data)=>{
-        if(err){
-            reject(err)
-
-        }
-        else{
-            resolve(data)
-        }
-    })
-})
+function getData(fileName) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(fileName, 'utf-8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        const array = data.toString().split('\n');
+        resolve(array);
+      }
+    });
+  });
 }
-console.log(getData("seed/fruits.txt").then(console.log))
-console.log(getDirectoryFiles("seed").then(console.log))
+console.log(getData('seed/fruits.txt').then(console.log));
+ //console.log(getDirectoryFiles('seed').then(console.log));
 
-module.exports={getData,getDirectoryFiles}
+module.exports = { getData, getDirectoryFiles };

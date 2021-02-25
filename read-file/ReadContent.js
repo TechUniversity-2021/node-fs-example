0481d1656757fefb5efca33ac8f7d79f57fc669d
srishtirathi <srishti.cs17@bmsce.ac.in>
2021-02-25 08:13:43 +0530

    add(read-file): add function to make object with key as filenames and value as array of content

--- read-file/ReadContent.js
+++ read-file/ReadContent.js
@@ -1,71 +1,22 @@
-const fs=require("fs")
 const path = require('path');
-const { compileFunction } = require("vm");
+const fileUtils = require('../file-utils/FileUtils');
 
-const {getData,getDirectoryFiles}=require("../file-utils/FileUtils")
+// const { name } = path.parse(fileName);
 
-async function readContent(dirName){
-  
-    const fileNames=await getDirectoryFiles(dirName)
-    //console.log(fileNames)
-    const fileData=fileNames.map(async (fileName)=>{
+const getData = async (dirName) => {
+  const fileNames = await fileUtils.getDirectoryFiles(dirName);
+  // console.log(fileNames)
 
-        const name=path.parse(fileName).name;
-       //console.log(fileName)
-        //console.log(name)
+  const result = {};
+  for (let i = 0; i < fileNames.length; i++) {
+    const fileName = path.parse(fileNames[i]).name;
+    // console.log(fileName)
+    const fileContent = await fileUtils.getData(`./seed/${fileNames[i]}`);
+    // console.log(fileContent)
+    // console.log(fileName,fileContent)
+    result[fileName] = fileContent;
+  }
+  return result;
+};
 
-        const content=await getData(`${dirName}/${fileName}`)
-        return content;
-        //console.log("i am  here")
-
-        //result[name]=content;
-
-     
-
-    })
-    const allPromises=await Promise.all(fileData)
-
-    //console.log(allPromise)
-    //console.log(fileData)
-    //console.log("hii")
-return allPromises;
-
-}
-
-readContent("seed").then(console.log)
-
-
-
-/*
-const fs = require("fs")
-const path = require('path');
-function readFiles(directory) {
-    return new Promise((resolve, reject) => {
-        fs.readdir(directory, (err, fileNames) => {
-            var result = {}
-            if (!err) {
-                fileNames.forEach(fileName => {
-
-                    const name = path.parse(fileName).name;
-
-                    fs.readFile("./seed/" + fileName, function read(err, data) {
-                        if (err) {
-                            throw err;
-                        }
-                        //console.log(name, data)
-                        result[name] = data
-                    });
-                })
-                resolve(result);
-
-            }
-            else {
-                reject(err)
-            }
-        })
-    })
-}
-
-console.log(readFiles("./seed").then(console.log))
-*/
-
+ //getData('seed').then(console.log)
